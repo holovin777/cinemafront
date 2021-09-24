2b751fa24395546d08f0691628c336821c979ab8
Viktor Holovin <holovin@mail.com>
2021-09-24 20:18:42 +0200

    Add styles

--- sitegen.py
+++ sitegen.py
@@ -1,9 +1,11 @@
 import os
-from jinja2 import Template
+import subprocess
+from jinja2 import Environment, FileSystemLoader
 import urllib.request, json
 
 articles_api = []
 api_url = input("Your API url: ")
+
 with urllib.request.urlopen(api_url) as url:
     data = json.loads(url.read().decode())
     i = 0
@@ -12,9 +14,10 @@ with urllib.request.urlopen(api_url) as url:
         i += 1
 
 site_name_input = input("Your site name: ")
-jinja = open("jinja2.html", "r")
-template = Template(jinja.read())
+env = Environment(loader = FileSystemLoader("./"))
+template = env.get_template("jinja2.html")
 if not os.path.isdir(".www"):
     os.mkdir(".www")
 index = open(".www/index.html", "w")
 index.write(template.render(site_name = site_name_input, articles = articles_api))
+subprocess.run(["cp", "styles.css", ".www/styles.css"])
