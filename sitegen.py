48c6b7a19d6ac1afb5fb2add8b561e85d38c1dd9
Viktor Holovin <holovin@mail.com>
2021-09-24 23:54:35 +0200

    Add collapse

--- sitegen.py
+++ sitegen.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 from jinja2 import Environment, FileSystemLoader
 import urllib.request, json
 
@@ -16,8 +15,10 @@ with urllib.request.urlopen(api_url) as url:
 site_name_input = input("Your site name: ")
 env = Environment(loader = FileSystemLoader("./"))
 template = env.get_template("jinja2.html")
+styles = env.get_template("jinja2styles.css")
 if not os.path.isdir(".www"):
     os.mkdir(".www")
-index = open(".www/index.html", "w")
-index.write(template.render(site_name = site_name_input, articles = articles_api))
-subprocess.run(["cp", "styles.css", ".www/styles.css"])
+index_output = open(".www/index.html", "w")
+index_output.write(template.render(site_name = site_name_input, articles = articles_api))
+styles_output = open(".www/styles.css", "w")
+styles_output.write(styles.render( articles = articles_api))
